605348a424550763ccbd13443295a8216176125c
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2016-03-25 10:57:16 -0600

    Add construction of force vector.

---
+++ libs/force_vector.m
@@ -0,0 +1,12 @@
+% f = force_vector(model)
+%
+%   This function generates the modified force vector for use with the
+%   modified stiffness matrix.
+%
+function f = force_vector(model)
+    f = zeros(numel(model.nodes)*model.dimensions, 1);
+    for force = model.forces
+        idx = (force.node-1)*model.dimensions + [1:model.dimensions];
+        f(idx) = force.vector(1:model.dimensions);
+    end
+end
