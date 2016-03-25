3fa57d68eb56f3d19ec302007f526a913b4dbd12
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2016-03-25 10:45:56 -0600

    Add modification of stiffness mat for constraints.

---
+++ libs/modify_stiffness_matrix.m
@@ -0,0 +1,14 @@
+function K = modify_stiffness_matrix(model, K)
+    for i = 1:numel(model.nodes)
+        node = model.nodes(i);
+        idx = [1:model.dimensions] + model.dimensions*(i-1);
+        constraint = model.constraints.(node.constraint);
+        constraint = constraint(1:model.dimensions);
+        constraint_idx = idx(constraint);
+        for j = constraint_idx
+            K(j,:) = 0;
+            K(:,j) = 0;
+            K(j,j) = 1;
+        end
+    end
+end
--- truss.m
+++ truss.m
@@ -2,6 +2,7 @@ function K = truss(model_file)
     add_library_path();
     model = load_model(model_file);
     K = master_stiffness_matrix(model);
+    K = modify_stiffness_matrix(model, K);
 end
 
 
